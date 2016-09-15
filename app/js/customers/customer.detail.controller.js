c92601f39a102cd5732f957f7c3ff3b857f8b02b
Vincent Duong <vduong178@gmail.com>
2016-09-15 16:51:05 -0700

    Ready for testing

--- app/js/customers/customer.detail.controller.js
+++ app/js/customers/customer.detail.controller.js
@@ -13,17 +13,27 @@
         vm.title = 'CustomerDetailController';
 
         // variables
-        vm.customer = $stateParams.customer;
+        vm.customerId = $stateParams.customerId;
+        vm.customer;
         vm.reviewMode = true;
 
         // functions
         vm.deleteCustomer = deleteCustomer;
+        vm.getCustomerById = getCustomerById;
 
         activate();
 
         ////////////////
 
         function activate() {
+        }
+
+        function deleteCustomer() {
+
+        }
+
+        function getCustomerById() {
+            
         }
     }
 })();
